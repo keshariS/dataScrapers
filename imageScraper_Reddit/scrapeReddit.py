9ea5bed4f3be1541c4e1ea64f4d7d8817ae6e2b7
SHUVAM KESHARI <skeshari1998@gmail.com>
2022-11-18 02:56:35 -0600

    Add files via upload

---
+++ imageScraper_Reddit/scrapeReddit.py
@@ -0,0 +1,102 @@
+import praw
+import requests
+import cv2
+import numpy as np
+import os
+import pickle
+
+from utils.create_token import create_token
+
+
+
+
+POST_SEARCH_AMOUNT = 5000 # --------
+scale_percent = 10 # percent of original image resolution (for saving the scraped image)------
+
+
+
+
+
+# Create directory if it doesn't exist to save images
+def create_folder(image_path):
+    CHECK_FOLDER = os.path.isdir(image_path)
+    # If folder doesn't exist, then create it.
+    if not CHECK_FOLDER:
+        os.makedirs(image_path)
+
+# Path to save images
+dir_path = os.path.dirname(os.path.realpath(__file__))
+image_path = os.path.join(dir_path, "images/")
+ignore_path = os.path.join(dir_path, "ignore_images/")
+create_folder(image_path)
+
+# Get token file to log into reddit.
+# You must enter your....
+# client_id - client secret - user_agent - username password
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+else:
+    creds = create_token()
+    pickle_out = open("token.pickle","wb")
+    pickle.dump(creds, pickle_out)
+
+reddit = praw.Reddit(client_id=creds['client_id'],
+                    client_secret=creds['client_secret'],
+                    user_agent=creds['user_agent'],
+                    username=creds['username'],
+                    password=creds['password'])
+
+
+
+
+f_final = open("sub_list.csv", "r") # file containing subreddits to scrape from----------
+
+
+
+img_notfound = cv2.imread('imageNF.png')
+for line in f_final:
+    sub = line.strip()
+    subreddit = reddit.subreddit(sub)
+    # image_path = os.path.join(image_path, "{sub}/") #----create separate folders for subreddits
+
+    print(f"Scraping from subreddit!: {sub}")
+    count = 0
+    for submission in subreddit.top(limit=POST_SEARCH_AMOUNT):
+        if "jpg" in submission.url.lower() or "png" in submission.url.lower():
+            try:
+                resp = requests.get(submission.url.lower(), stream=True).raw
+                image = np.asarray(bytearray(resp.read()), dtype="uint8")
+                image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+                # Transforms on images: resize, to lower resolution for faster training
+
+                width = int(image.shape[1] * scale_percent / 100)
+                height = int(image.shape[0] * scale_percent / 100)
+                dim = (width, height)
+  
+                # resize image
+                resized_img = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+                compare_image = cv2.resize(image,(224,224))
+
+                # Get all images to ignore
+                for (dirpath, dirnames, filenames) in os.walk(ignore_path):
+                    ignore_paths = [os.path.join(dirpath, file) for file in filenames]
+                ignore_flag = False
+
+                for ignore in ignore_paths:
+                    ignore = cv2.imread(ignore)
+                    difference = cv2.subtract(ignore, compare_image)
+                    b, g, r = cv2.split(difference)
+                    total_difference = cv2.countNonZero(b) + cv2.countNonZero(g) + cv2.countNonZero(r)
+                    if total_difference == 0:
+                        ignore_flag = True
+
+                if not ignore_flag:
+                    cv2.imwrite(f"{image_path}{sub}-{submission.id}.jpg", resized_img)
+                    count += 1
+                    
+            except Exception as e:
+                print(f"Image failed. {submission.url.lower()}")
+                print(e)
+        
