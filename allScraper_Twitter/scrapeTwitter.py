848b7835325135c1176cddfa463bbb3e1b5dba52
keshariS <skeshari1998@gmail.com>
2023-02-05 05:20:20 +0000

    cleaned

--- allScraper_Twitter/scrapeTwitter.py
+++ allScraper_Twitter/scrapeTwitter.py
@@ -1,7 +1,7 @@
 import twint
 
-out_dir = "/mnt/smdata/twitter/"
-f = open("all_hashtags1.csv", "r")
+out_dir = "/mnt/smdata/twitter/VAMoS/" #output directory for the csv files
+f = open("all_hashtags.csv", "r")
 for line in f:
     h = line.strip("#\n")
     print(f"Scraping: #{h}...")
@@ -9,7 +9,7 @@ for line in f:
     c = twint.Config()
     c.Hide_output = True
     c.Search = f"#{h}"
-    c.Limit = 40 #increments of 20
+    #c.Limit = 40 # number of tweets to scrape: increments of 20
     c.Output = f"{out_dir}{h}.csv"
     c.Store_csv = True
     c.Count = True
