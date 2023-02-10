7924f9c5b2823f9f455e05d6176ab1c3c5c801b8
keshariS <skeshari1998@gmail.com>
2023-02-10 19:10:28 +0000

    cleaned

--- allScraper_Twitter/csv2media.py
+++ allScraper_Twitter/csv2media.py
@@ -16,7 +16,10 @@ htags = f"{target}hashtags/"
 imgs = f"{target}images/"
 
 #read each file and create respective folder
+fcnt = 0
+imcnt = 0
 for file in os.listdir(htags):
+    fcnt = fcnt +1
     #print(file)
     df = pd.read_csv(f"{htags}{file}")
     folder = file.split(".csv")
@@ -24,7 +27,7 @@ for file in os.listdir(htags):
     create_folder(os.path.join(imgs, folder))
     
     photos = df.photos
-    print(f"[+] Downloading images to {folder}/...")
+    print(f"[{fcnt}] Downloading images to {folder}/...")
     count = 0
     for pics in photos:
         pics = pics.strip("[]")
@@ -34,5 +37,10 @@ for file in os.listdir(htags):
             if(pic!=""):
                 #print(pic)
                 count = count +1
-                wget.download(pic, out = f"{imgs}{folder}/", bar = None) # visit each link and download to respective folder
+                try:
+                    wget.download(pic, out = f"{imgs}{folder}/", bar = None) # visit each link and download to respective folder
+                except:
+                    continue
     print(f"[-] {count} images downloaded.")
+    imcnt = imcnt + count
+print(f"[-] Total images downloaded: {imcnt}")
