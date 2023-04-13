04aaf2e6fcc1bbab63f5d621dfa295773cc22074
keshariS <skeshari1998@gmail.com>
2023-04-13 04:20:19 +0000

    mordecai stuff added, geonames deleted

---
+++ allScraper_Twitter/geo_freq.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv(f"topicModels/eng_esp_GEO_cat1.csv", engine='python')
+
+for category in range (2,7):
+    df1 = pd.read_csv(f"topicModels/eng_esp_GEO_cat{category}.csv", engine='python')
+    df1 = df1.drop(df1.index[0])
+    df = pd.concat([df, df1], axis=0)
+
+#print(df.head)
+country_counts = df["country"].value_counts()
+#print(country_counts)
+country_counts.to_csv("topicModels/GEO_country_freq.csv")
+print("Saved country counts")
+
+usa = df.loc[df['country'] == "USA"]
+#print(usa)
+usa = usa.dropna()
+#print(usa)
+state_counts = usa["state"].value_counts()
+#print(state_counts)
+state_counts.to_csv("topicModels/GEO_state_freq.csv")
+print("Saved state counts")
